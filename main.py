71ba9c72c2d2ef81c3a542b37b435f6a0ac2a421
Gabriel Reis <gabrielreis@vixdynamics.com.br>
2023-11-15 10:44:40 -0300

    set ip server

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from vidstream import CameraClient
 import threading
 import time
 
-sending = CameraClient('ip pra quem recebe')
+sending = CameraClient('192.168.15.2')
 
 th = threading.Thread(target=sending.start_stream)
 th.start()
