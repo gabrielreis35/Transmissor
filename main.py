8070ec66d256106adb2b6de2bb11d3d70303d2c1
Gabriel Reis <89395144+gabrielreis35@users.noreply.github.com>
2023-12-04 17:38:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from vidstream import CameraClient
 import threading
 import time
 
-sending = CameraClient('192.168.15.2')
+sending = CameraClient('192.168.15.2', 8000)
 
 th = threading.Thread(target=sending.start_stream)
 th.start()
